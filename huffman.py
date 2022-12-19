1b05fdfa1f95273bd7c421db161a05ecc8058098
Ben Donkin <benjamindonkin@gmail.com>
2022-12-19 15:43:03 -0400

    Completed "genFreqTable" method in huffman.py Currently uses dictionary but a more space effiecient data structure may be necessary

--- huffman.py
+++ huffman.py
@@ -8,25 +8,32 @@ class Huffman:
     # == TO DO ==
     #  - set up error handling for fileIO
     def __init__(self,inputFile,outputFile):
-        self.inputFile = self.readFile(inputFile)
-        self.outputFile = open(outputFile,'x')
+        self.inputFile = open(inputFile)
+        self.outputFile = open(outputFile,'w')
+        self.freqTable = self.genFreqTable()
+            
 
-    def generateFreqTable(self):
+    # Create a frequency table for the initialized input file
+    def genFreqTable(self):
         dict = {}
 
         while True:
             c = self.inputFile.read(1)
-            if c is '':
+            if c == '':
                 return dict
-            isRepeat = True if dict[c]
+            try:
+                dict[c]+=1
+            # if c does not exist in the table yet then create a new index
+            except KeyError:
+                dict[c]=1
+    
+    # Create a Huffman code tree for the initialized input file
+    def genHuffmanCode(self):
 
-        return 
-
-    def readFile():
         return
 
-file = open(sys.argv[1])
-print(file.read(1))
-print(file.read(1))
-print(file.read(1))
-print(file.read(1))
+
+
+h = Huffman(sys.argv[1],"fqwedqw")
+c = h.genFreqTable()
+print(c)
